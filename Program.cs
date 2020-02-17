d2bb3e546e6fe521bc21eaac87266e8ecf7c3055
Pedro Pereira <pmh.pereira@gmail.com>
2020-02-17 01:25:39 +0000

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace HomeQuest
+{
+    public class Program
+    {
+        public static bool Debug { get; set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Home Builder v{0} for Quest", Assembly.GetExecutingAssembly().GetName().Version.ToString(2));
+            Console.WriteLine();
+
+            if (args.Length == 1 && DebugRequired(args[0]))
+                Debug = true;
+
+            var app = new Application();
+            try
+            {
+                app.Run();
+                Console.WriteLine(@"Success.");
+            }
+            catch(Exception e)
+            {
+                Console.Error.WriteLine(@"Failed with error: {0}", e.Message);
+
+                if (Debug)
+                    Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                app.Reset();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(@"Press any key to EXIT.");
+            Console.ReadLine();
+        }
+
+        private static bool DebugRequired(string param)
+        {
+            return param == "-d" || param == "--debug" || param == "/D" || param == "/d";
+        }
+    }
+}
